8daa7a062f686d4d6cba83957505f5f9ec8c8a58
Aadil Shah <adilns786@gmail.com>
2025-01-14 00:03:21 +0530

    update

--- backend/app.py
+++ backend/app.py
@@ -8,8 +8,7 @@ from dotenv import load_dotenv
 
 # Load environment variables from .env file
 load_dotenv()
-GEMINI_API_KEY = "AIzaSyAul6uqEPAHYvHxK0i5a-Oot9w99nDFF60"
-# os.getenv("GEMINI_API_KEY")
+GEMINI_API_KEY =  os.getenv("GEMINI_API_KEY")
 
 app = FastAPI()
 
